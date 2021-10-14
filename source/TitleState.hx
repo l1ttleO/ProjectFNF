b5aa0a37ea49b8e38e3b9b305591779cdb0a0b8c
Stilic <63605602+Stilic@users.noreply.github.com>
2021-10-14 07:53:49 +0200

    Update source/TitleState.hx
    Co-authored-by: l1ttleO <l1ttleofficial@outlook.com>

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -305,7 +305,7 @@ class TitleState extends MusicBeatState
 
 	function addMoreText(text:String, ?offset:Float = 0)
 	{
-		if (textGroup != null) {
+		if(textGroup != null) {
 			var coolText:Alphabet = new Alphabet(0, 0, text, true, false);
 			coolText.screenCenter(X);
 			coolText.y += (textGroup.length * 60) + 200 + offset;
