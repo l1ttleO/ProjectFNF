1ddb5c2ac193413f5e3c371a4a6713acccf025e6
Stilic <lateman-jpeg@outlook.fr>
2021-10-14 07:20:55 +0200

    Revert "add advanced info bar option and version text"
    This reverts commit 5af4519562cb4bc8e3d9421c21c46155c3941840.

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -23,13 +23,9 @@ class CoolUtil
 		['Hard', '-hard']
 	];
 
-	public static function difficultyString(upper:Bool=true):String
+	public static function difficultyString():String
 	{
-		if (upper) {
-			return difficultyStuff[PlayState.storyDifficulty][0].toUpperCase();
-		} else {
-			return difficultyStuff[PlayState.storyDifficulty][0];
-		}
+		return difficultyStuff[PlayState.storyDifficulty][0].toUpperCase();
 	}
 
 	public static function boundTo(value:Float, min:Float, max:Float):Float {
