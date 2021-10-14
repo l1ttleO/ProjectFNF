ea679aa9b32774a7914b05247a564ad4447212a6
Stilic <63605602+Stilic@users.noreply.github.com>
2021-10-14 07:54:02 +0200

    Update source/TitleState.hx
    Co-authored-by: l1ttleO <l1ttleofficial@outlook.com>

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -99,7 +99,7 @@ class TitleState extends MusicBeatState
 		#elseif CHARTING
 		MusicBeatState.switchState(new ChartingState());
 		#else
-		if (FlxG.save.data.flashing == null && !FlashingState.leftState) {
+		if(FlxG.save.data.flashing == null && !FlashingState.leftState) {
 			FlxTransitionableState.skipNextTransIn = true;
 			FlxTransitionableState.skipNextTransOut = true;
 			MusicBeatState.switchState(new FlashingState());
