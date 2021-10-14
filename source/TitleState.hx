fb0120e541b2e6b7c906e0a018ed59457d7d9bc7
Stilic <63605602+Stilic@users.noreply.github.com>
2021-10-14 07:39:51 +0200

    Update source/TitleState.hx
    Co-authored-by: l1ttleO <l1ttleofficial@outlook.com>

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -136,8 +136,7 @@ class TitleState extends MusicBeatState
 	{
 		if (!initialized)
 		{
-			if (FlxG.sound.music == null)
-			{
+			if(FlxG.sound.music == null) {
 				FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
 
 				FlxG.sound.music.fadeIn(4, 0, 0.7);
