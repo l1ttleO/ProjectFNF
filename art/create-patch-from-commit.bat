e6a2f83207904a3fa27d7793b6c22006621fe0c9
l1ttleO <l1ttleofficial@outlook.com>
2022-06-27 14:20:13 +0500

    Fix counter in 'create-patch-from-commit.bat'

--- art/create-patch-from-commit.bat
+++ art/create-patch-from-commit.bat
@@ -1,7 +1,8 @@
+@echo off
 echo Creating patch from the latest commit
 cd ..
 set cnt=1
-for %%A in (*) do set /a cnt+=1
+for %%A in ('dir /b patches\source') do set /a cnt+=1
 git format-patch --start-number %cnt% -o patches/source -1 HEAD
 echo.
 echo done.
