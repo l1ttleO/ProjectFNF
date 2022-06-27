4dbd8d09edf19c0b8acff2a0014e68fc179423a7
l1ttleO <l1ttleofficial@outlook.com>
2022-06-27 22:39:59 +0500

    'Play as Opponent' AND THE FUCKING COUNTER

--- art/create-patch-from-commit.bat
+++ art/create-patch-from-commit.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Creating patch from the latest commit
 cd ..
-set cnt=1
+set cnt=2
 for %%A in ('dir /b patches\source') do set /a cnt+=1
 git format-patch --start-number %cnt% -o patches/source -1 HEAD
 echo.
